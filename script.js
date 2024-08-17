43cb7dfeff8b2d2b01a99282e6fc8f4b8f88e587
Abraham Sackey Ohene Gyan <151635418+iamodlj@users.noreply.github.com>
2024-08-17 22:51:12 +0000

    git add .

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
-// script.js
-
-const cursor = document.querySelector('.coding-cursor');
-
 function moveCursor() {
-    let width = window.innerWidth;
-    let height = window.innerHeight;
-
-    let newX = Math.random() * width;
-    let newY = Math.random() * height;
-
-    cursor.style.transform = `translate(${newX}px, ${newY}px)`;
-
-    setTimeout(moveCursor, 1000);
-}
-
-moveCursor();
+    const cursor = document.querySelector('.coding-cursor');
+    const x = Math.floor(Math.random() * window.innerWidth);
+    const y = Math.floor(Math.random() * window.innerHeight);
+    cursor.style.left = `${x}px`;
+    cursor.style.top = `${y}px`;
+  }
+  
+  setInterval(moveCursor, 1000);
+  
